ac650d8475970fed9bcf908ae61b8573a0837b72
WONHO22 <dnjsgh2122@gmail.com>
2023-08-01 11:57:57 +0900

    CI/CD 테스트2

--- my-agora-states-client-react/src/components/Form.js
+++ my-agora-states-client-react/src/components/Form.js
@@ -24,7 +24,7 @@ export const Form = ({ addDiscussion }) => {
             <textarea
               id="story"
               name="story"
-              placeholder="질문을 작성하세요22"
+              placeholder="질문을 작성하세요33"
               required
             ></textarea>
           </div>
